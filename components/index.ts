136f8ddbd90a209269fae4e56dd697a7227df407
SmartBanker84 <simone.capo@yahoo.it>
2025-06-18 23:32:54 +0200

    Create index.ts

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+// components/index.ts
+export { KPICard, KPIGrid, KPIGridWithLoading, CompactKPICard } from './KPI'
+export { MonthlyChart, ChartWrapper } from './MonthlyChart'
+export { CVList } from './CVList'
+export { AddCandidateModal } from './AddCandidateModal'
+export { ManagerDashboard } from './ManagerDashboard'
+export { RecruitingDashboard } from './RecruitingDashboard'
