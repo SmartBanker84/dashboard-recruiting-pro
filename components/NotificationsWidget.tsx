69123378cf997ad382c87f914c7e46b866cb9f82
SmartBanker84 <simone.capo@yahoo.it>
2025-06-21 17:56:00 +0200

    Create NotificationsWidget.tsx

---
+++ components/NotificationsWidget.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function NotificationsWidget({ userId }: { userId: string }) {
+  return (
+    <div className="p-4 bg-white rounded shadow">
+      <div>Notifiche rapide (widget placeholder)</div>
+    </div>
+  )
+}
