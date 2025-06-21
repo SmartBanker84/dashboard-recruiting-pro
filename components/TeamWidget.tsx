bbdbc72549cfcb5963fc1ac19ab68fdd1001fa51
SmartBanker84 <simone.capo@yahoo.it>
2025-06-21 17:56:28 +0200

    Create TeamWidget.tsx

---
+++ components/TeamWidget.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function TeamWidget({ userId }: { userId: string }) {
+  return (
+    <div className="p-4 bg-white rounded shadow">
+      <div>Team widget (placeholder)</div>
+    </div>
+  )
+}
