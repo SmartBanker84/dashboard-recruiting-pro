0074ec010abe64eaf43bc01f4c7d8ebff23c4334
SmartBanker84 <simone.capo@yahoo.it>
2025-06-22 11:29:45 +0200

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,8 +79,8 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="preconnect" href="https://xyz.supabase.co" />
-        <link rel="dns-prefetch" href="//xyz.supabase.co" />
+        <link rel="preconnect" href="https://hhyloiuzgaggednuuern.supabase.co" />
+        <link rel="dns-prefetch" href="//hhyloiuzgaggednuuern.supabase.co" />
         <script src="/scripts/theme.js" />
         {/* <script defer src="/_vercel/insights/script.js"></script> */}
       </head>
